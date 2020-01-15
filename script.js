74f32c10c43a258e5e7625e4d5343f485b8ad4a1
Ben Kioko <benedictmkioko@gmail.com>
2020-01-15 18:31:15 -0500

    Change grid to 16x16

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   let newDiv = document.createElement('div');
   newDiv.addEventListener('mouseenter', () => {
     newDiv.classList.add('sketched');
