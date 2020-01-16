594cfa8452fef5bef5557a453a6c21840bccb212
Ben Kioko <benedictmkioko@gmail.com>
2020-01-15 22:24:52 -0500

    Add resize feature Add button that, when clicked, prompts the user to input a value representing the number of divs in one row of the grid that they wish to have. Change 'grid-template-columns' and 'grid-template-rows' to account for the user input as an inline style. Run createDivs function to make a grid of userInput by userInput number of divs, then reset the grid.

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const container = document.querySelector('.container');
 const reset = document.querySelector('#reset');
+const resize = document.querySelector('#resize');
 
-for (let i = 0; i < 256; i++) {
-  let newDiv = document.createElement('div');
-  newDiv.addEventListener('mouseenter', () => {
-    newDiv.classList.add('sketched');
-  });
-  container.appendChild(newDiv);
-}
+createDivs(256);
 
 reset.addEventListener('click', resetGrid);
+resize.addEventListener('click', resizeGrid);
 
-function resetGrid () {
+function resetGrid() {
   const nodeList = document.getElementsByTagName('div');
   const squares = [];
-  // change nodeList from nodelist to array
+  // change nodeList from node list to array
   for(let i = nodeList.length; i--; squares.unshift(nodeList[i]));
   squares.forEach(function (square) {
     square.classList.remove('sketched');
   });
+}
+
+function createDivs(num) {
+  for (let i = 0; i < num; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.addEventListener('mouseenter', () => {
+      newDiv.classList.add('sketched');
+    });
+    container.appendChild(newDiv);
+  }
+
+}
+
+function resizeGrid() {
+   let gridLength = prompt('How many boxes would you like each row to have?', 16);
+   container.style = `grid-template-columns: repeat(${gridLength}, auto);
+                      grid-template-rows: repeat(${gridLength}, auto)`;
+   createDivs(gridLength * gridLength);
+   resetGrid();
 }
